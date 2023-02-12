96f4c687f77e0ded4cb6f696a6b2e74cff009fb3
Matthew Miles <matthewmiles2@gmail.com>
2023-02-12 04:11:56 +0000

    Item Counters (but not quite how I want)

--- Assets/Scripts/Powerups.cs
+++ Assets/Scripts/Powerups.cs
@@ -1,27 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Powerups : MonoBehaviour
 {
     public enum Types { FireRate, DamageReduction, Speed, Knockback, Thorns, Burn, Slow, GunUpgrade, BulletStorm, Vaccine };
     [System.NonSerialized] public Dictionary<Types, Powerup> powerups = new Dictionary<Types, Powerup>();
-    // private Dictionary<Types, int> powerupCounts = new Dictionary<Types, int>();
+    private Dictionary<Types, int> powerupCounts = new Dictionary<Types, int>();
+    private Dictionary<Types, Text> powerupTexts = new Dictionary<Types, Text>();
+    private Dictionary<Types, Image> powerupIcons = new Dictionary<Types, Image>();
+
+    public Color activeColor = Color.white;
+    public Color inactiveColor = Color.gray;
 
     public Player player;
-
-    public Types referenceDELETEME;
 
     void Start()
     {
         foreach (Powerup powerup in GetComponentsInChildren<Powerup>())
         {
             powerups.Add(powerup.type, powerup);
+            powerupTexts.Add(powerup.type, powerup.GetComponentInChildren<Text>());
+            powerupIcons.Add(powerup.type, powerup.GetComponentInChildren<Image>());
         }
 
         foreach (Types type in System.Enum.GetValues(typeof(Types)))
         {
-            // powerupCounts.Add(type, 0);
+            powerupCounts.Add(type, 0);
+            powerupTexts[type].enabled = false;
+            powerupIcons[type].color = inactiveColor;
             if (!powerups.ContainsKey(type))
             {
                 Debug.LogError("Powerups.cs: Powerup type " + type + " is missing.");
@@ -49,6 +57,9 @@ public class Powerups : MonoBehaviour
     public void AddPowerup(Types type)
     {
         powerups[type].ApplyPowerup(player, player.hand.weapon);
-        // powerupCounts[type]++;
+        powerupCounts[type]++;
+        powerupTexts[type].enabled = true;
+        powerupTexts[type].text = powerupCounts[type].ToString();
+        powerupIcons[type].color = activeColor;
     }
 }
